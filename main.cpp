0e6e065496f2578c331e8f5c0303144570aa37ac
Benjamin Gluck <b.gluck132@gmail.com>
2025-02-08 16:43:20 -0500

    add setup files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHServo.h>
+#include <FEHAccel.h>
+
+int main()
+{
+    LCD.Write("Hello, world!");
+}
