05295f7ced615a94d7e8a4f5fa5f1d976f7f12ed
Benjamin Gluck <b.gluck132@gmail.com>
2025-03-06 14:35:28 -0500

    init gh pages repo

--- src/constants.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/constants.h"
-
-Motor motorA = Motor(FEHMotor::Motor0, 9, FEHIO::P0_0);
-Motor motorB = Motor(FEHMotor::Motor1, 9, FEHIO::P0_1);
-Motor motorC = Motor(FEHMotor::Motor2, 9, FEHIO::P0_2);
-
-AnalogInputPin cdsCell(FEHIO::P1_1);
