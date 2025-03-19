36a4eb119c262e5dec48fb64eb77e11112b78184
glucko <60188659+glucko@users.noreply.github.com>
2025-03-19 16:38:39 +0000

    Deploying to gh-pages from @ glucko/feh-robot@9291329cf9a417a99c2dfff1095922771eebda83 🚀

--- classLogger.js
+++ classLogger.js
@@ -5,6 +5,7 @@ var classLogger =
     [ "getPrimaryEncoderInfo", "classLogger.html#a68d162994519b0ebe175f57ee8165e2d", null ],
     [ "getSecondaryEncoderInfo", "classLogger.html#abb44843fe8923e240fb987cee4cf4583", null ],
     [ "log", "classLogger.html#a2aa660e693bcd2688c239d8f8a76f8f0", null ],
+    [ "logWithDelay", "classLogger.html#abd0cca10d065e54bd1db7856602ac3e7", null ],
     [ "turnLoggerOff", "classLogger.html#af3aed0260332b738a072fc7856abd5be", null ],
     [ "turnLoggerOn", "classLogger.html#ae773ec18d0c508993ae7597d9d00cc05", null ]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['log_0',['log',['../classLogger.html#a2aa660e693bcd2688c239d8f8a76f8f0',1,'Logger']]],
-  ['logger_1',['Logger',['../classLogger.html#af2e6f83cadd6f578bbfbe5482a78ff6e',1,'Logger']]]
+  ['logger_1',['Logger',['../classLogger.html#af2e6f83cadd6f578bbfbe5482a78ff6e',1,'Logger']]],
+  ['logwithdelay_2',['logWithDelay',['../classLogger.html#abd0cca10d065e54bd1db7856602ac3e7',1,'Logger']]]
 ];
