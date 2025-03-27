aafb2bd90c6dcf7f5789562b4660af5b07452c58
Benjamin Gluck <b.gluck132@gmail.com>
2025-03-27 15:58:01 -0400

    add logging to navigation

--- src/navigation.cpp
+++ src/navigation.cpp
@@ -10,6 +10,8 @@ void hitButton(int rev = 1)
     drive.driveDirection(-2, Direction::AB);
 
     drive.driveDirection(1, Direction::AB);
+
+    logger.log("finished hitButton()");
 }
 
 void pickUpBucket()
@@ -24,56 +26,59 @@ void pickUpBucket()
     drive.driveDirection(-15, Direction::AB);
 
     drive.turn(45);
+
+    logger.log("finished pickUpBucket()");
 }
 
 void dropOffBucket()
 {
     drive.driveDirection(12, Direction::AB);
     servo.SetDegree(SERVO_LOWERED);
+
+    logger.log("finished dropOffBucket()");
 }
 
 void driveUpRamp(int rev)
 {
     drive.turn(45);
     drive.driveDirection(rev * 33, Direction::AB);
+
+    logger.log("finished driveUpRamp()");
 }
 
 void flipFertilizer(bool flipUp)
 {
     if (flipUp)
-    {
         servo.SetDegree(SERVO_LOWERED);
-    }
     else
-    {
         servo.SetDegree(SERVO_RAISED);
-    }
 
     switch (RCS.GetCorrectLever())
     {
         // Left lever
     case 0:
+        logger.log("Flipping left lever");
         drive.driveDirection(-2, Direction::BC);
         break;
 
         // Middle lever
     case 1:
+        logger.log("Flipping middle lever");
         break;
 
         // Right lever
     case 2:
+        logger.log("Flipping right lever");
         drive.driveDirection(2, Direction::BC);
         break;
     }
 
     if (flipUp)
-    {
         servo.SetDegree(SERVO_RAISED);
-    }
     else
-    {
         servo.SetDegree(SERVO_LOWERED);
-    }
 
     drive.driveDirection(2, Direction::AB);
+
+    logger.log("finished flipFertilizer()");
 }
