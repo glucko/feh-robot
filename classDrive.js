b8023b5ff5b228e5ee9e4ba085b9820ae3fdec6d
glucko <60188659+glucko@users.noreply.github.com>
2025-03-28 16:35:42 +0000

    Deploying to gh-pages from @ glucko/feh-robot@37dfb77ed5bed98f356c75e858fc58333f3dcdab 🚀

--- classDrive.js
+++ classDrive.js
@@ -2,5 +2,5 @@ var classDrive =
 [
     [ "Drive", "classDrive.html#a2fae4083755da292e3d82637306851c5", null ],
     [ "driveDirection", "classDrive.html#ae55bc49816b1f8262641c2b61eb3cc7a", null ],
-    [ "turn", "classDrive.html#a1a694f6fdf49162df147bfa313c2270a", null ]
+    [ "turn", "classDrive.html#adcd1d81ec972b8aa51a03e8dd5d6dfff", null ]
 ];
--- search/functions_b.js
+++ search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['turn_0',['turn',['../classDrive.html#a1a694f6fdf49162df147bfa313c2270a',1,'Drive']]],
+  ['turn_0',['turn',['../classDrive.html#adcd1d81ec972b8aa51a03e8dd5d6dfff',1,'Drive']]],
   ['turnloggeroff_1',['turnLoggerOff',['../classLogger.html#af3aed0260332b738a072fc7856abd5be',1,'Logger']]],
   ['turnloggeron_2',['turnLoggerOn',['../classLogger.html#ae773ec18d0c508993ae7597d9d00cc05',1,'Logger']]]
 ];
