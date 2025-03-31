12cc76ddd486e358aeba0828072c9c5f11585b8a
wisam <wisamftw@gmail.com>
2025-03-31 13:30:05 -0400

    turn more

--- src/navigation.cpp
+++ src/navigation.cpp
@@ -80,7 +80,7 @@ void driveUpRamp(int rev)
 
     drive.driveDirection(-21, Direction::AB);
 
-    drive.turn(92);
+    drive.turn(95);
     drive.driveDirection(rev * 33, Direction::AB, 35);
 
     logger.log("finished driveUpRamp()");
