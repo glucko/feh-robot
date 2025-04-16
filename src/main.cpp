ddeb643e0e8f9ca65c5bd28b202ed6ca0a5648c4
wisam <wisamftw@gmail.com>
2025-04-16 11:38:51 -0400

    doc

--- src/main.cpp
+++ src/main.cpp
@@ -15,23 +15,38 @@ void ERCMain()
     Drive drive = Drive();
 
     waitUntilTouch();
+    // Drive to Apple Bucket
+    drive.driveToPosition({-2, -1, 0}, 25);
+
+    // Drive to Apple Bucket
     drive.driveToPosition({12, 16, 0}, 25);
 
+    // Drive to ramp
     drive.driveToPosition({6, -1, 0}, 25);
 
+    // Drive up ramp to table
     drive.driveToPosition({30, -1, 0}, 35);
 
+    // Drive to window
     drive.driveToPosition({25, 5, 0}, 25);
 
+    // Open window
     drive.driveToPosition({25, 15, 0}, 25);
 
+    // Drive to and hit button
     drive.driveToPosition({27, 20, 0}, 25);
 
+    // Drive to top of ramp
     drive.driveToPosition({30, -1, 0}, 25);
 
+    // Drive down ramp
     drive.driveToPosition({6, -1, 0}, 25);
 
+    // Drive to compost
     drive.driveToPosition({-5, 10, 0}, 25);
+
+    // Drive to start button
+    drive.driveToPosition({-2, -1, 0}, 25);
 
     /*drive.driveToPosition({-2, -1, 0}, 25);
     drive.driveToPosition({10, 15, 0}, 25);
